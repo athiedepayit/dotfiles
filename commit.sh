26d1c7768833b0a73f284714d746296bf353aa92
Adam Thiede <athiede@payitgov.com>
2024-09-17 09:21:23 -0500

    update

---
+++ commit.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cp "$HOME"/.config/alacritty/* .
+cp "$HOME"/.config/tmux/tmux.conf tmux.conf
+cp "$HOME/.config/nvim/init.lua" nvim_init.lua 
+cp "$HOME"/.config/aerospace/* .
+cp "$HOME/.vimrc" vimrc
+cp "$HOME/.bashrc" bashrc
+cp "$HOME/Library/Application Support/Code/User/settings.json" .
+git add .
+git commit -m "update $(date +%Y%m%d %H:%M)"
