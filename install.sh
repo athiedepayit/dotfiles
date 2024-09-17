eb1655663b86a42187270e203694fee1feab823a
Adam Thiede <athiede@payitgov.com>
2024-09-17 08:49:53 -0500

    update install script

--- install.sh
+++ install.sh
@@ -4,21 +4,33 @@ for dir in $config_dirs; do
 	mkdir -p "$HOME/.config/$dir"
 done
 
-# alacritty
-cp alacritty.toml "$HOME/.config/alacritty/"
-cp dark.toml "$HOME/.config/alacritty/"
-cp light.toml "$HOME/.config/alacritty/"
-# tmux
-cp tmux.conf "$HOME/.config/tmux/tmux.conf"
-# nvim
-cp nvim_init.lua "$HOME/.config/nvim/init.lua"
-# aerospace
-cp aerospace.toml "$HOME/.config/aerospace/"
-# vim
-cp vimrc "$HOME/.vimrc"
-# bashrc
-cp bashrc "$HOME/.bashrc"
+install_dots() {
+	# alacritty
+	cp alacritty.toml "$HOME/.config/alacritty/"
+	cp dark.toml "$HOME/.config/alacritty/"
+	cp light.toml "$HOME/.config/alacritty/"
+	# tmux
+	cp tmux.conf "$HOME/.config/tmux/tmux.conf"
+	# nvim
+	cp nvim_init.lua "$HOME/.config/nvim/init.lua"
+	# aerospace
+	cp aerospace.toml "$HOME/.config/aerospace/"
+	# vim
+	cp vimrc "$HOME/.vimrc"
+	# bashrc
+	cp bashrc "$HOME/.bashrc"
+	# vscode
+	mkdir -p "$HOME/Library/Application Support/Code/User/"
+	cp settings.json "$HOME/Library/Application Support/Code/User/"
+}
 
-# vscode
-mkdir -p "$HOME/Library/Application Support/Code/User/"
-cp settings.json "$HOME/Library/Application Support/Code/User/"
+brew_packages() {
+	# brew install
+	# generated with brew list --installed-on-request
+	brew install $(cat brew_packages)
+	# generated with brew list --cask
+	brew install --cask $(cat brew_casks)
+}
+
+#install_dots
+brew_packages
